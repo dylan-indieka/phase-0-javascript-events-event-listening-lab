7c365886684e5f86196ff1a6225f0e883e82e0fd
dylan-indieka <dylan.indieka@student.moringaschool.com>
2025-03-17 04:22:27 -0700

    add files

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
-}
+    const button = document.getElementById('button');
+    button.addEventListener('click', () => {
+      alert('Button was clicked!');
+    });
+  }
+  
+  addingEventListener();
+
